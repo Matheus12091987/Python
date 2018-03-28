158b79f0fcaa65286ffca61b55450df0604f383c
Matheus <matheus.peres01@fatec.sp.gov.br>
2018-03-28 17:32:02 -0300

    Subindo o python

---
+++ Aprendendo_Python2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 21:42:34 2018
+
+@author: matheus
+"""
+
+salario = input("Digite seu salario:")
+fgts = (float(salario)/100)*8.5
+print ("Voce vai pagar de fundo de garantia o valor de: R$", fgts)
