4205768c1370c96a0bd0e3ce690feb4386293eb1
Matheus <matheus.peres01@fatec.sp.gov.br>
2018-06-12 23:11:22 -0300

    Finalizando modelo da folha em pdf

--- Eurofarma/Gerando PDF.py
+++ Eurofarma/Gerando PDF.py
@@ -14,106 +14,35 @@ resultadoD = "Aprovado"
 resultadoFinal = "Lote Liberado para Produção"
 #teste='lalala'
 
-html = ("""
-    
-    <p align="center"> 
-    Eurofarma Laboratórios SA
-    </p>
- 
-    <p align="center"> 
-    Relatório de inicio de Produção
-    </p> 
- 
-    <p align="left"> 
-    Valor Procurado: """ + teste + """
-    </p>
- 
-    <p></p>
-
-    <div>
-
-         <div> 
-
-         Bolsa Posição A: """ + resultadoA + """
-
-         </div>
-    
-         <div>
-
-         <img alt="html image example" src="teste.jpg" width="150" height="75">
-
-         </div>      
-     
-     </div>
-     
-     <p></p>
-     
-     <div>
-     
-         <div> 
-
-         Bolsa Posição B: """ + resultadoB + """
-
-         </div>
-
-         <div>
-         
-         <img alt="html image example" src="teste1.jpg" width="150" height="75">
-         
-         </div>
-         
-     </div>
-     
-     <p></p>
-    
-     <div>
-         <div>
-         
-         Bolsa Posição C: """ + resultadoC + """
-
-         </div>
- 
-         <div>
-         
-         <img alt="html image example" src="teste3.jpg" width="150" height="75">
-
-         </div>
-
-     </div>
-     
-     <p></p>
-     
-     <div>
-
-         <div> 
-         
-         Bolsa Posição D: """ + resultadoD + """
-
-         </div>
- 
-         <div>
-         
-         <img alt="html image example" src="teste2.jpg" width="150" height="75">
-
-         </div>
-
-     </div>
-     
-     <p align="left"> 
-	Avaliação Final: """ + resultadoFinal + """
-	</p>
-     
-     <p></p><p></p>
-          
-     <p align="left"> 
-	______________________                                                                              ______________________     
-	</p>
-     
-     <p align="left"> 
-	  Assinatura do Operador                                                                             Assinatura do Lider do Processo
-	</p>
-     
-     """)
+html = ("""<head>Pagina 1 de 1</head>
+<p align="center"><h1>Eurofarma Laboratórios SA</h1></p>
+<p align="center"><h2>Relatório de Início de Produção</h2></p>
+<p align="left">  Valor Procurado: """ + teste + """</p>
+<p></p>
+<div>
+<div><div>Bolsa Posição A: """ + resultadoA + """                                                                      </div>
+<div><img alt="html image example" src="teste.jpg" width="150" height="75"></div>
+</div> 
+<p></p>
+<div>
+<div>Bolsa Posição B: """ + resultadoB + """                                                                      </div>
+<div><img alt="html image example" src="teste1.jpg" width="150" height="75"></div>
+</div>
+<p></p>
+<div>
+<div>Bolsa Posição C: """ + resultadoC + """                                                                      </div>
+<div><img alt="html image example" src="teste3.jpg" width="150" height="75"></div>
+</div>
+<p></p>
+<div>
+<div>Bolsa Posição D: """ + resultadoD + """                                                                      </div>
+<div><img alt="html image example" src="teste2.jpg" width="150" height="75"></div>
+</div>
+</div>
+<p align="left">Avaliação Final: """ + resultadoFinal + """</p>
+<p></p><p></p>
+<p align="left">	______________________                                                                              ______________________     </p>
+<p align="left">	  Assinatura do Operador                                                                             Assinatura do Lider do Processo</p>""")
 
 class MyFPDF(FPDF, HTMLMixin):
     pass
