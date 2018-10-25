fd93dfb4923dd7df5997631ca50c974c27be480e
Minicurso Django Turma 01 <django1@fatecrp.local>
2018-10-24 21:02:22 -0300

    django

---
+++ Learning Django/teste.py
@@ -0,0 +1,9 @@
+import os               #importando a biblioteca de controle do SO
+
+os.system ('cls')       #enviando um clear para o prompt
+
+print('teste')          #imprimindo ma tela
+
+x = "Ola tudo bem!!"    #criando variaveis
+
+print(x)                #imprmindo as variaveis
